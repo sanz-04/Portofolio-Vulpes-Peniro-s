dc3c059655520dcc6350adb1a9e10e9aff673cef
Vulpes--poniros <ikhsannuraddin1976@gmail.com>
2026-06-02 08:23:13 +0700

    ubah base name lagi

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     // GANTI 'nama_repository_kamu' dengan nama repo GitHub-mu (contoh: '/portofolio/')
-    base: '/Portofolio-Vulpes-Peniro-s/', 
+    base: './', 
   }
 })
